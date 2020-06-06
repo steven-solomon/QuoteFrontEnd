98905e35dc83ae90f90ee0b466a1f28e344f16d5
Steven Solomon <solomon.steven.m@gmail.com>
2020-06-06 15:11:02 -0400

    Refactor use navigation
    - Add @react-navigation and its dependencies
    - Add react-native-gesture-handler/jestSetup so tests would run
    - Add fileMock for the images included by react-native-gesture-handler

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import SearchView from './src/SearchView';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <SearchView />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Add Stock" component={SearchView} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 
